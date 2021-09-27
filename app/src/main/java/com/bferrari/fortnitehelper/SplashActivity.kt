830a11cf6196964f99ad51976e87608a38b6fe6e
Bruno Ferrari <bruno.ferrari@enjoei.com.br>
2021-09-27 17:33:42 -0300

    Add app icon

--- app/src/main/java/com/bferrari/fortnitehelper/SplashActivity.kt
+++ app/src/main/java/com/bferrari/fortnitehelper/SplashActivity.kt
@@ -3,15 +3,17 @@ package com.bferrari.fortnitehelper
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.*
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -22,23 +24,41 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
-class MainActivity : AppCompatActivity() {
+@ExperimentalAnimationApi
+class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContent { SplashScreen() }
     }
 
     @Preview
     @Composable
     fun SplashScreen() {
+        val animState = remember {
+            MutableTransitionState(false).apply {
+                targetState = true
+            }
+        }
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(colorResource(id = R.color.bg))
+                .background(colorResource(id = R.color.fortnite_primary))
         )
-        SplashMessage(
-            message = Message("Fortnite", "Helper")
-        )
+
+        AnimatedVisibility(
+            visibleState = animState,
+            enter = fadeIn() + expandVertically(expandFrom = Alignment.CenterVertically),
+            exit = fadeOut() + shrinkOut()
+        ) {
+            Column {
+                SplashMessage(
+                    message = Message("Fortnite", "Helper")
+                )
+            }
+        }
     }
 
     @Composable
